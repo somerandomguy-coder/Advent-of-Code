68545628adbb00690eb570923ed8fc2e1cf4fccc
somerandomguy-coder <153010587+somerandomguy-coder@users.noreply.github.com>
2024-01-20 17:51:17 +1100

    added part 2 solution

--- AoC2023/day2/day2_p2.py
+++ AoC2023/day2/day2_p2.py
@@ -1,14 +1,26 @@
-def is_possible(game):
+def power_of_fewest_ball(game):
+    max_red = 0
+    max_blue = 0
+    max_green = 0
     ball_sets = game.split("; ")
     for ball_set in ball_sets:
         lst = ball_set.split(", ")
         for i in lst:
             num, color = i.split()
-            if color == "red" and int(num) > 12:
-                return False
-            if color == "green" and int(num) > 13:
-                return False
-            if color == "blue" and int(num) > 14:
-                return False
-    return True
+            num = int(num)
+            if color == "red" and num > max_red:
+                max_red = num
+            if color == "green" and num > max_green:
+                max_green = num
+            if color == "blue" and num > max_blue:
+                max_blue = num
+    return max_red * max_green * max_blue
     
+
+ans = 0
+filename = open(r"day2/day2_input.txt", "r")
+for line in filename:
+    ids, games = line.split(": ")
+    ans += power_of_fewest_ball(games)
+print(ans)
+
