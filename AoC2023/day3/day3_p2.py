6dadd4b702cc6da9722969922e2563c3e96b3d8a
somerandomguy-coder <153010587+somerandomguy-coder@users.noreply.github.com>
2024-02-11 01:57:20 +1100

    Progressing on solution for day3_p2

--- AoC2023/day3/day3_p2.py
+++ AoC2023/day3/day3_p2.py
@@ -1,84 +1,137 @@
+
+
 def adjacent_number(r, c): #row, column
     adjacent = 0
     #first row
     if r == 0:
         if matrix[r][c-1].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r) +" "+ str(c-1)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r) +" "+ str(c-1)]["index"])
         if matrix[r][c+1].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r) +" "+ str(c+1)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r) +" "+ str(c+1)]["index"])
         if matrix[r+1][c+1].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r+1) +" "+ str(c+1)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r+1) +" "+ str(c+1)]["index"])
         if matrix[r+1][c].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r+1) +" "+ str(c)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r+1) +" "+ str(c)]["index"])
         if matrix[r+1][c-1].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r+1) +" "+ str(c-1)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r+1) +" "+ str(c-1)]["index"])
         return adjacent
     #last row
     if r == len(matrix)-1:
         if matrix[r-1][c-1].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r-1) +" "+ str(c-1)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r-1) +" "+ str(c-1)]["index"])
         if matrix[r][c-1].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r) +" "+ str(c-1)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r) +" "+ str(c-1)]["index"])
         if matrix[r-1][c].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r-1) +" "+ str(c)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r-1) +" "+ str(c)]["index"])
         if matrix[r-1][c+1].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r-1) +" "+ str(c+1)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r-1) +" "+ str(c+1)]["index"])
         if matrix[r][c+1].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r) +" "+ str(c+1)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r) +" "+ str(c+1)]["index"])
         return adjacent
         
     #first column
     if c == 0:
         if matrix[r-1][c].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r-1) +" "+ str(c)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r-1) +" "+ str(c)]["index"])
         if matrix[r-1][c+1].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r-1) +" "+ str(c+1)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r-1) +" "+ str(c+1)]["index"])
         if matrix[r][c+1].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r) +" "+ str(c+1)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r) +" "+ str(c+1)]["index"])
         if matrix[r+1][c+1].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r+1) +" "+ str(c+1)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r+1) +" "+ str(c+1)]["index"])
         if matrix[r+1][c].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r+1) +" "+ str(c)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r+1) +" "+ str(c)]["index"])
         return adjacent
     #last column
     if c == len(matrix[0])-1:
         if matrix[r-1][c-1].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r-1) +" "+ str(c-1)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r-1) +" "+ str(c-1)]["index"])
         if matrix[r][c-1].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r) +" "+ str(c-1)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r) +" "+ str(c-1)]["index"])
         if matrix[r-1][c].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r-1) +" "+ str(c)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r-1) +" "+ str(c)]["index"])
         if matrix[r+1][c].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r+1) +" "+ str(c)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r+1) +" "+ str(c)]["index"])
         if matrix[r+1][c-1].isnumeric() == True:
-            adjacent += 1
+            if dic[str(r+1) +" "+ str(c-1)]["index"] in index_set:
+                adjacent += 1
+                index_set.remove(dic[str(r+1) +" "+ str(c-1)]["index"])
         return adjacent
     #other normal object
     if matrix[r-1][c-1].isnumeric() == True:
-        adjacent += 1
+        if dic[str(r-1) +" "+ str(c-1)]["index"] in index_set:
+            adjacent += 1
+            index_set.remove(dic[str(r-1) +" "+ str(c-1)]["index"])
     if matrix[r][c-1].isnumeric() == True:
-        adjacent += 1
+        if dic[str(r) +" "+ str(c-1)]["index"] in index_set:
+            adjacent += 1
+            index_set.remove(dic[str(r) +" "+ str(c-1)]["index"])
     if matrix[r-1][c].isnumeric() == True:
-        adjacent += 1
+        if dic[str(r-1) +" "+ str(c)]["index"] in index_set:
+            adjacent += 1
+            index_set.remove(dic[str(r-1) +" "+ str(c)]["index"])
     if matrix[r-1][c+1].isnumeric() == True:
-        adjacent += 1
+        if dic[str(r-1) +" "+ str(c+1)]["index"] in index_set:
+            adjacent += 1
+            index_set.remove(dic[str(r-1) +" "+ str(c+1)]["index"])
     if matrix[r][c+1].isnumeric() == True:
-        adjacent += 1    
+        if dic[str(r) +" "+ str(c+1)]["index"] in index_set:
+            adjacent += 1
+            index_set.remove(dic[str(r) +" "+ str(c+1)]["index"])  
     if matrix[r+1][c+1].isnumeric() == True:
-        adjacent += 1
+        if dic[str(r+1) +" "+ str(c+1)]["index"] in index_set:
+            adjacent += 1
+            index_set.remove(dic[str(r+1) +" "+ str(c+1)]["index"])
     if matrix[r+1][c].isnumeric() == True:
-        adjacent += 1
+        if dic[str(r+1) +" "+ str(c)]["index"] in index_set:
+            adjacent += 1
+            index_set.remove(dic[str(r+1) +" "+ str(c)]["index"])
     if matrix[r+1][c-1].isnumeric() == True:
-        adjacent += 1
+        if dic[str(r+1) +" "+ str(c-1)]["index"] in index_set:
+            adjacent += 1
+            index_set.remove(dic[str(r+1) +" "+ str(c-1)]["index"])
     return adjacent
 
 
 
-def take_number(row, column, long):
-    number = "0"
-    for num in range(long):
-        number+=matrix[row][column+num]
-    return int(number[1:])
 
 filename = open(r"day3_test_input2.txt", "r")
 matrix = filename.read().split()
@@ -87,6 +140,7 @@ for i in range(len(matrix)):
 
 #create dictionary
 dic = {}
+index_set = set()
 for i in range(len(matrix)):
     j = 0
     while j < len(matrix[0]):
@@ -96,7 +150,7 @@ for i in range(len(matrix)):
             while cur_index < len(matrix[0]):
                 if matrix[i][cur_index].isnumeric() == True:
                     dic[str(i)+" "+str(j+jump)] = {"index":str(i)+" "+str(j)}
-                    # dic[str(i)+" "+str(j)] = {"long":jump+1}
+                    index_set.add(str(i)+" "+str(j))
                     cur_index += 1
                     jump+=1
                 else: 
@@ -109,7 +163,6 @@ for i in range(len(matrix)):
 for i in range(len(matrix)):
     for j in range(len(matrix[0])):
         if matrix[i][j] == "*":
-            print(adjacent_number(i,j))
-            if adjacent_number(i,j) == 2:
+            adjac = adjacent_number(i,j)
+            if adjac == 2:
                 print(i, j, "is gear")
-print(dic)
